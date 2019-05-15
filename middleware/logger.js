9640352dd68052b7f186d82933c990ba7dfc3a0f
RaneWallin <rane@ranewallin.com>
2019-05-14 18:04:25 -0700

    fix date parsing

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,7 +2,7 @@ module.exports = (req, res, next) => {
     console.log(`
     URL: http://localhost:4000${req.originalUrl}
     Type: ${req.method}
-    Time: ${Date.now()}
+    Time: ${new Date(Date.parse(Date()))}
     `);
     next();
 }
