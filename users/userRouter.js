5e5a5dd4d7399ef3831d71e0f608f6f3a2083b56
RaneWallin <rane@ranewallin.com>
2019-05-14 19:03:49 -0700

    GET /api/user/

--- users/userRouter.js
+++ users/userRouter.js
@@ -16,7 +16,32 @@ router.post('/', validateBody, validateUser, async (req, res) => {
     }
 });
 
-router.post('/:id/posts', validateUserId, async (req, res) => {
+// router.post('/:id/posts', validateUserId, async (req, res) => {
+//     try {
+//         const posts = await userDb.getUserPosts(req.user.id)
+//
+//         if (posts) return res.status(200).json(posts);
+//         else return res.status(400).json({ message: "Could not retrieve the posts"})
+//     } catch(err) {
+//         res.status(500).json(err)
+//     }
+// });
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await userDb.get();
+
+        res.json(users);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', validateUserId, (req, res) => {
+
+});
+
+router.get('/:id/posts', validateUserId, async (req, res) => {
     try {
         const posts = await userDb.getUserPosts(req.user.id)
 
@@ -25,18 +50,6 @@ router.post('/:id/posts', validateUserId, async (req, res) => {
     } catch(err) {
         res.status(500).json(err)
     }
-});
-
-router.get('/', (req, res) => {
-
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
 });
 
 router.delete('/:id', (req, res) => {
