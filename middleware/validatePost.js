f07253a66295cde299c4f9231fa4d15126328df3
RaneWallin <rane@ranewallin.com>
2019-05-14 18:19:47 -0700

    completed validatePost and pulling body validation into validateBody

---
+++ middleware/validatePost.js
@@ -0,0 +1,7 @@
+modules.export = (req, res, next) => {
+    const { body } = req;
+
+    if(!body.text) return res.status(400).json({ message: "Missing post data" });
+
+    next();
+}
--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -1,8 +1,6 @@
 module.exports = (req, res, next) => {
     const { body } = req;
 
-    if (!body) return res.status(400).json({ message: "Missing user data"});
-
     if(!body.name) return res.status(400).json({ message: "Missing required field: name "})
 
     next();
