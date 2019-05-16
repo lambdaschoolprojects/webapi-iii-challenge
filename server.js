7b70dc1b1d8d43befc47401b0996ccaa6429f334
RaneWallin <rane@ranewallin.com>
2019-05-15 19:50:35 -0700

    Merge branch 'rane-wallin'
    # Conflicts:
    #	posts/postRouter.js
    #	server.js
    #	users/userRouter.js

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const userRouter = require('./users/userRouter');
-const postRouter  = require('./posts/postRouter');
+const express = require("express");
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
 
 const server = express();
 server.use(express.json());
-const logger = require('./middleware/logger');
+const logger = require("./middleware/logger");
 
 server.use(logger);
 
-server.use('/api/user', userRouter);
-server.use('/api/post', postRouter);
+server.use("/api/user", userRouter);
+server.use("/api/post", postRouter);
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
-
 
 module.exports = server;
